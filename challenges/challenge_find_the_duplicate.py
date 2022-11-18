d3ab511a163ef8088108dc9a6737aa3a604f0525
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-18 18:36:59 -0300

    feat: find duplicate
    first try, without success

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,30 @@
+from challenges.merge_sort import merge_sort
+
+
+def validate_nums(nums, list_size):
+    if nums[0] < 0 or list_size < 1:
+        raise ValueError
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    target = 0
+    next_target = 1
+
+    try:
+        list_size = len(nums)
+
+        merge_sort(nums)
+        validate_nums(nums, list_size)
+
+        for _ in range(list_size):
+            if nums[target] == nums[next_target]:
+                return nums[target]
+            else:
+                move = 1
+                target += move
+                next_target += move
+
+    except (TypeError, IndexError, ValueError):
+        pass
+
+    return False
