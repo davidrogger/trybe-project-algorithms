5d9689d70f0bb4ad75d2eeb462e2f669eb827237
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-18 17:04:50 -0300

    feat: iterative palindrome

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_element = 0
+    last_element = len(word) - 1
+
+    if not word:
+        return False
+
+    while first_element <= last_element:
+        if word[first_element] != word[last_element]:
+            return False
+        else:
+            first_element += 1
+            last_element -= 1
+
+    return True
